eca692703c80d51444d85a525e2b71fb28832dfb
EliasBinde <34208547+EliasBinde@users.noreply.github.com>
2022-05-16 17:54:20 +0200

    INIT

---
+++ index.js
@@ -0,0 +1,32 @@
+import { config } from "dotenv";
+import cron from "node-cron";
+import { updateUserDB } from "./update.js";
+
+config();
+
+const users = [
+  {
+    userKey: process.env.NOTION_KEY,
+    databaseId: process.env.NOTION_DATABASE_ID,
+    timeToUpdate: "17:55",
+    timezone: "Europe/Berlin",
+  },
+];
+
+const main = async () => {
+  await updateUserDB(user.userKey, user.databaseId);
+};
+
+for (const user of users) {
+  const [hour, minute] = user.timeToUpdate.split(":");
+  cron.schedule(
+    `${minute} ${hour} * * *`,
+    () => {
+      main(user);
+    },
+    {
+      scheduled: true,
+      timezone: "Europe/Berlin",
+    }
+  );
+}
---
+++ update.js
@@ -0,0 +1,63 @@
+import { Client } from "@notionhq/client";
+import moment from "moment-timezone";
+
+export const updateUserDB = async (userKey, databaseID) => {
+  const notion = new Client({ auth: userKey });
+  const databaseId = databaseID;
+
+  const getTasks = async () => {
+    const res = (
+      await notion.databases.query({
+        database_id: databaseId,
+      })
+    ).results;
+    return res;
+  };
+
+  const checkIfRecurring = (task) => {
+    const type = task?.properties?.Type?.formula?.string;
+    if (!task?.properties?.Due?.date?.start) return false;
+    return type === "Recurring";
+  };
+
+  const updateInDb = async (task, newDue) => {
+    const res = await notion.pages.update({
+      page_id: task.id,
+      properties: {
+        Done: {
+          checkbox: false,
+        },
+        Due: {
+          date: {
+            start: newDue,
+            time_zone: "Europe/Berlin",
+          },
+        },
+      },
+    });
+  };
+
+  const updateTask = async (task) => {
+    const today = new Date();
+    const due = new Date(task.properties.Due.date.start);
+    if (!(due.toDateString() === today.toDateString())) return;
+
+    const interval = task?.properties?.Interval?.number;
+
+    const newDue = moment
+      .utc(due)
+      .tz("Europe/Berlin")
+      .add(interval, "days")
+      .format("YYYY-MM-DDTHH:mm:ss");
+    console.log("Updating task:", task.id);
+    updateInDb(task, newDue);
+  };
+
+  const updateRecurringTasks = async () => {
+    const tasks = await getTasks();
+    const recurringTasks = tasks.filter((task) => checkIfRecurring(task));
+    for (let task of recurringTasks) updateTask(task);
+  };
+
+  updateRecurringTasks();
+};
